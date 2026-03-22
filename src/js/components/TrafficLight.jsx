e5325ca08039ddf4db14f271b4c489fda9a3a157
unknown <adielrocaflores42@gmail.com>
2026-03-22 18:04:56 +0100

    hecho

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,46 @@
+// import React from "react";
+
+import { useState } from "react"
+
+
+const TrafficLight = () => {
+
+    const[color,setColor] = useState("verde");
+
+        const cambiarColor = ()=>{
+            if (color === "rojo") {
+                setColor("verde")
+            }
+
+            if (color === "verde") {
+            setColor("amarillo")
+            }
+
+            if (color === "amarillo") {
+            setColor("rojo")
+            }
+        }
+
+        // setInterval(cambiarColor, 3000);
+
+        return (
+        <div className="align-items-center justify-content-center d-flex flex-column mx-auto mt-4 bg-dark" style={{width:"100px", height:"200px"}}>
+
+            <button  type="button"
+  className={`btn rounded-circle ${color === "rojo" ? "bg-danger" : "bg-secondary"}`}
+  style={{ width: "60px", height: "60px" }}
+  onClick={() => setColor("rojo")}> A</button>
+            <button type="button"
+  className={`btn rounded-circle ${color === "amarillo" ? "bg-warning" : "bg-secondary"}`}
+  style={{ width: "60px", height: "60px" }}
+  onClick={() => setColor("amarillo")}>B</button>
+            <button  type="button"
+  className={`btn rounded-circle ${color === "verde" ? "bg-success" : "bg-secondary"}`}
+  style={{ width: "60px", height: "60px" }}
+  onClick={() => setColor("verde")}> C</button>
+        </div>
+        )
+}
+
+
+export default TrafficLight
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,10 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import TrafficLight from './components/TrafficLight';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+  <TrafficLight/>
   </React.StrictMode>,
 )
